6ce1f807c7d6a222da826b914f8e29a24502c3e0
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-11-13 19:54:19 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,5 +3,5 @@
 
 api_id = "22475741"
 api_hash = "1a217be71a0225e0a678af286c211f8a"
-bot_token = "7347471600:AAFWOe1YpS1Ap_AqWpLNHZNY4V-Tkj3nUTE"
+bot_token = "7748512042:AAEZIzvgMp5na6gp-O2u6ZM7MFQzH9C3rUk"
 # Bot Username = "https://t.me/SmexyTxtLeechingBot"
